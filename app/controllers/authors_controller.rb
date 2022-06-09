de9772aac3d5c6a01df4b4881e1574c0bf07232f
Zachary Sirna <zsirna@yahoo.com>
2022-06-09 14:49:25 -0400

    My Version

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,14 +1,13 @@
 class AuthorsController < ApplicationController
+  rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_enitity
   
   def show
     author = Author.find(params[:id])
-
     render json: author
   end
 
   def create
-    author = Author.create(author_params)
-
+    author = Author.create!(author_params)
     render json: author, status: :created
   end
 
@@ -18,4 +17,8 @@ class AuthorsController < ApplicationController
     params.permit(:email, :name)
   end
   
+  def render_unprocessable_enitity(invalid)
+    render json: {errors: invalid.record.errors}, status: :unprocessable_entity
+  end
+
 end
